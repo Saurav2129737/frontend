26a8ec1c619b32c68e98ec9bf27755fcf3301e29
Saurav2129737 <sauravshirvastav831@gmail.com>
2024-08-17 01:42:48 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Welcome to Zeesh Resturent  where culinary excellence meets warm
+              hospitality. Our chefs craft delicious, seasonal dishes using the
+              freshest ingredients. Whether you're here for a casual meal or a
+              special celebration, we promise an unforgettable dining experience
+              in a cozy, inviting atmosphere. Join us and savor the flavors of
+              [Cuisine Type].
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Saurav Shrivastav</div>
+          <div className="right">
+            <p>Saurav Shrivastav Mohali Punjab</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Saurav Shrivastav</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Saurav Shrivastav.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
